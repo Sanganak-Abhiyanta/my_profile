fc3f687c6f51de58a25967d3c30e6754b47a2dad
Nitish Kumar <imsanganakabhiyanta@gmail.com>
2023-05-18 15:16:49 +0530

    Experience Section Completed.

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -5,7 +5,7 @@ import Card from "./Card";
 export default function Resume() {
   return (
     <section className="resume container section" id="resume">
-      <h2 className="section_title">Experience</h2>
+      <h2 className="section__title">Experience</h2>
 
       <div className="resume_container grid">
         <div className="timeline grid">
