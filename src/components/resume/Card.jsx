69e32a2361d4666cae77da3313cc369e13cb3978
Nitish Kumar <imsanganakabhiyanta@gmail.com>
2023-05-18 13:07:32 +0530

    Starting for the resume page.

---
+++ src/components/resume/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Card(props) {
+  return (
+    <div className="timeline__item">
+        <i className={props.icon} ></i>
+        <span className="timeline__date">{props.year} </span>
+        <h3 className="timeline__title">{props.tilte} </h3>
+        <p className="timeline_text">{props.desc} </p>
+    </div>
+  )
+}
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
-import "./resume.css"
-import Data from "./Data"
+import React from "react";
+import "./resume.css";
+import Data from "./Data";
+import Card from "./Card";
 export default function Resume() {
   return (
     <section className="resume container section" id="resume">
       <h2 className="section_title">Experience</h2>
 
       <div className="resume_container grid">
-        <div className="timeline grid"></div>
+        <div className="timeline grid">
+          {Data.map((val, id) => {
+            if (val.category === "education") {
+              return (
+                <Card
+                  key={id}
+                  icon={val.icon}
+                  tilte={val.title}
+                  year={val.year}
+                  desc={val.desc}
+                />
+              );
+            }
+          })}
+        </div>
+
+        <div className="timeline grid">
+          {Data.map((val, index) => {
+            if (val.category === "experience") {
+              return (
+                <Card
+                  key={index}
+                  icon={val.icon}
+                  tilte={val.title}
+                  year={val.year}
+                  desc={val.desc}
+                />
+              );
+            }
+          })}
+        </div>
       </div>
     </section>
-  )
+  );
 }
